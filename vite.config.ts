607183eca109a16e8034073d5f4037ee8ed34961
jine021012 <jine021012>
2025-08-10 00:04:29 +0900

    자드가자1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
@@ -11,4 +13,5 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  
 });
